1fdb17d9f8e338414b4f573a28832a48acedd0a8
Alger <AlgerDu@outlook.com>
2018-11-27 21:47:44 +0800

    服务的控制

--- D.DeployTool.GuarderFactory.WinService/WinServiceGuarder.cs
+++ D.DeployTool.GuarderFactory.WinService/WinServiceGuarder.cs
@@ -2,7 +2,9 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
+using System.Management;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -58,6 +60,80 @@ namespace D.DeployTool
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// win service 可执行文件路径是否正确，平时经常遇到安装错了
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private bool IsExePathRight(string name, string path)
+        {
+            using (
+                ManagementObject wmiService
+                = new ManagementObject("Win32_Service.Name='" + name + "'")
+                )
+            {
+                wmiService.Get();
+                string currentserviceExePath = wmiService["PathName"].ToString();
+
+                return currentserviceExePath == path;
+            }
+        }
+
+        /// <summary>
+        /// 安装服务
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private bool InstallService(string path)
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[]
+                    {
+                        path
+                    });
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"安装 WinService {path} 出现异常：{ex}");
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 卸载服务
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private bool UninstallService(string path)
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[]
+                    {
+                        "/U",
+                        path
+                    });
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"卸载 WinService {path} 出现异常：{ex}");
+
+                return false;
+            }
+        }
+
+        private bool RunService()
+        {
+
         }
     }
 }
